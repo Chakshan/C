cb4c10466f88d4208a24dd21df90ddd14ec0138a
Chakshan <chakshan.kothakota@gmail.com>
2024-04-23 21:51:20 -0700

    hashmap interface

---
+++ Hashmap/hashmap.h
@@ -0,0 +1,23 @@
+#ifndef _HASHMAP_H_
+#define _HASHMAP_H_
+
+#include <stdbool.h>
+
+typedef struct {
+	void *key;
+	void *value;
+} entry_t;
+
+typedef struct {
+	entry_t *hash_table;
+	int size;
+	int (*hash)(void *);
+} hashmap_t;
+
+void init(hashmap_t *map, int min_capacity, int (*hash)(void*));
+void insert(hashmap_t *map, void *key, void *value);
+void *remove(hashmap_t *map, void *key);
+void *get(hashmap_t *map, void *key);
+void clear(hashmap_t *map);
+
+#endif
